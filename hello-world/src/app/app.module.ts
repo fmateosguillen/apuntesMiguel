c960f06b69ab36bdce1ff20f13d68615c4b1aae3
Francisco Javier Mateos Guillén <fmateosguillen@gmail.com>
2020-12-04 14:30:50 +0100

    04/12/2020 14:30

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentePruebaComponent } from './componente-prueba/componente-prueba.component';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentePruebaComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
